f43cbec9a58fd448a069913b645906905d1de1c7
Objuret <jochen@wikman.com>
2025-06-19 22:48:20 +0200

    wq

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Define the main route and render the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Run the application (development mode)
 if __name__ == '__main__':
     app.run(debug=True)
